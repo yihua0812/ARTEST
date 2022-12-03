e97d3d828d2daf33bba30ebe0fa3655d349d574d
yihua0812 <112862214+yihua0812@users.noreply.github.com>
2022-12-03 11:51:38 +0800

    edit img

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/20221203.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
